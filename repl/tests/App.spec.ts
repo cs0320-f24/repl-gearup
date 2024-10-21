56b8d4d105b66e15899a62de41a649f9843d2a57
Nicole Sanchez <nicolesanchez@MacBook-Air-342.local>
2024-10-20 23:18:38 -0400

    gearup ready sol

--- repl/tests/App.spec.ts
+++ repl/tests/App.spec.ts
@@ -11,8 +11,8 @@ import { test, expect } from '@playwright/test';
 
 // If you needed to do something before every test case...
 test.beforeEach(() => {
-    // ... you'd put it here.
-  })
+  // ... you'd put it here.
+})
 
 /**
  * Don't worry about the "async" yet. We'll cover it in more detail
@@ -48,16 +48,34 @@ test('on page load, i see a button', async ({ page }) => {
 
 test('after I click the button, its label increments', async ({ page }) => {
   await page.goto('http://localhost:8000/');
-  await expect(page.getByRole('button', { name: 'Submitted 0 times'})).toBeVisible()
+  await expect(page.getByRole('button', { name: 'Submitted 0 times' })).toBeVisible()
   await page.getByRole('button', { name: 'Submitted 0 times' }).click();
   await page.getByRole('button', { name: 'Submitted 1 times' }).click();
   await page.getByRole('button', { name: 'Submitted 2 times' }).click();
   await page.getByRole('button', { name: 'Submitted 3 times' }).click();
-  await expect(page.getByRole('button', { name: 'Submitted 4 times'})).toBeVisible()
+  await expect(page.getByRole('button', { name: 'Submitted 4 times' })).toBeVisible()
 
 });
 
 // TODO: Fill this in to test your button push functionality!
 test('fetch works on different inputs', async ({ page }) => {
-  
+  await page.goto('http://localhost:8000/');
+  await page.getByPlaceholder('Enter command here!').click();
+  await page.getByPlaceholder('Enter command here!').fill('test');
+  await page.getByRole('button', { name: 'Submitted 0 times' }).click();
+  await expect(page.getByText('Error: Invalid input test')).toBeVisible();
+
+  await page.getByPlaceholder('Enter command here!').click();
+  await page.getByPlaceholder('Enter command here!').fill('4');
+  await page.getByRole('button', { name: 'Submitted 1 times' }).click();
+  await expect(page.getByText('Error: Invalid input 4')).toBeVisible();
+
+  await page.getByPlaceholder('Enter command here!').click();
+  await page.getByPlaceholder('Enter command here!').fill('3');
+  await page.getByRole('button', { name: 'Submitted 2 times' }).click();
+  await expect(page.getByText('Jlejandro Aackson')).toBeVisible();
+  await page.getByPlaceholder('Enter command here!').click();
+  await page.getByPlaceholder('Enter command here!').fill('0');
+  await page.getByRole('button', { name: 'Submitted 3 times' }).click();
+  await expect(page.getByText('Nim Telson')).toBeVisible();
 });
